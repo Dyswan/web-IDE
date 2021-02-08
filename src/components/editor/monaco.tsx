be4bdece4400449693d9c5d8d78e3d284f5b76d6
Apale7 <1092377056@qq.com>
2021-02-08 15:51:36 +0800

    community_edition done

--- src/components/editor/monaco.tsx
+++ src/components/editor/monaco.tsx
@@ -1,34 +1,32 @@
-import { useState } from "react";
-import useWindowSize from '../../hooks/windowSize';
+import useWindowSize from "../../hooks/windowSize";
 
-import MonacoEditor from 'react-monaco-editor';
-
-
+import MonacoEditor from "react-monaco-editor";
+import {getLanguage, storeCode} from "../../cache/cache";
 export default function Monaco(props: any) {
-    
-    const options = {
-        selectOnLineNumbers: true
-    };
-    const onChange = (newValue: string, e: any) => {
-        props.setCode(newValue);
-    }
-    const onMounted = (editor: any, monaco: any) => {
-        console.log('language', props.language);
-        editor.focus();
-    }
-    const size = useWindowSize()
-    return (
-        <div>
-            <MonacoEditor
-                width={size.innerWidth}
-                height={size.innerHeight*0.65}
-                language={props.language}
-                theme="vs-dark"
-                value={props.code}
-                options={options}
-                onChange={onChange}
-                editorDidMount={onMounted}
-            />
-        </div>
-    );
+  const options = {
+    selectOnLineNumbers: true
+  };
+  const onChange = (code: string, e: any) => {
+    props.setCode(code);
+    storeCode(getLanguage(), code)
+  };
+  const onMounted = (editor: any, monaco: any) => {
+    console.log("language", props.language);
+    editor.focus();
+  };
+  const size = useWindowSize();
+  return (
+    <div>
+      <MonacoEditor
+        width={size.innerWidth}
+        height={size.innerHeight * 0.65}
+        language={props.language}
+        theme="vs-dark"
+        value={props.code}
+        options={options}
+        onChange={onChange}
+        editorDidMount={onMounted}
+      />
+    </div>
+  );
 }
--- src/components/language_select/language_select.tsx
+++ src/components/language_select/language_select.tsx
@@ -1,21 +1,25 @@
-import { Select } from 'antd';
-const { Option } = Select;
-export default function LanguageSelect(props:any) {
-    const onChange = (value: string) => {
-        console.log(value);
-        props.setLanguage(value)
-    }
+import {Select} from 'antd';
+import {getCode, storeLanguage} from "../../cache/cache";
 
-    return (
-        <Select
-            onChange={onChange}
-            defaultValue="0"
-            style={{ width: 120 }}>
-            <Option value="0">C++</Option>
-            <Option value="1">Java</Option>
-            <Option value="2" disabled>
-                JavaScript</Option>
-            <Option value="3" disabled>Python</Option>
-        </Select>
-    )
+const {Option} = Select;
+export default function LanguageSelect(props: any) {
+  const onChange = (value: string) => {
+    storeLanguage(Number(value));
+    console.log(value);
+    props.setLanguage(value)
+    props.setCode(getCode(Number(value)))
+  }
+
+  return (
+    <Select
+      onChange={onChange}
+      value={props.value}
+      style={{width: 120}}>
+      <Option value="0">C++</Option>
+      <Option value="1">Java</Option>
+      <Option value="2" disabled>
+        JavaScript</Option>
+      <Option value="3" disabled>Python</Option>
+    </Select>
+  )
 }
